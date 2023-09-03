a6107bc4795b891e6c2c4f831111e9c6ab47a6c1
MSK4477 <ks7997067@gmail.com>
2023-09-04 00:51:38 +0530

    hi

--- deployment-task/edit.jsx
+++ deployment-task/edit.jsx
@@ -1,101 +1,101 @@
-// Edit.jsx
+// // Edit.jsx
 
-// eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from "react";
-import { useSearchParams, useNavigate } from "react-router-dom";
+// // eslint-disable-next-line no-unused-vars
+// import React, { useEffect, useState } from "react";
+// import { useSearchParams, useNavigate } from "react-router-dom";
 
-const Edit = () => {
-  const initialState = { name: "", age: "", image: "" };
-  const navigate = useNavigate();
-  const [searchParams] = useSearchParams();
-  const userId = searchParams.get("id");
+// const Edit = () => {
+//   const initialState = { name: "", age: "", image: "" };
+//   const navigate = useNavigate();
+//   const [searchParams] = useSearchParams();
+//   const userId = searchParams.get("id");
 
-  const [formData, setFormData] = useState(initialState);
+//   const [formData, setFormData] = useState(initialState);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:4000/api/user/${userId}`);
-        if (!response.ok) {
-          throw new Error("Failed to fetch user data");
-        }
-        const data = await response.json();
-        setFormData(data.data); // Assuming the user data is in a "data" field
-      } catch (error) {
-        console.error("Error fetching user data:", error);
-      }
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(`http://localhost:4000/api/user/${userId}`);
+//         if (!response.ok) {
+//           throw new Error("Failed to fetch user data");
+//         }
+//         const data = await response.json();
+//         setFormData(data.data); // Assuming the user data is in a "data" field
+//       } catch (error) {
+//         console.error("Error fetching user data:", error);
+//       }
+//     };
 
-    fetchData();
-  }, [userId]); // Include userId in the dependency array
+//     fetchData();
+//   }, [userId]); // Include userId in the dependency array
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
 
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
 
-  const sendReq = async (e) => {
-    e.preventDefault();
+//   const sendReq = async (e) => {
+//     e.preventDefault();
 
-    try {
-      const response = await fetch(`http://localhost:4000/api/user/${userId}`, {
-        method: "PUT",
-        body: JSON.stringify(formData),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
+//     try {
+//       const response = await fetch(`http://localhost:4000/api/user/${userId}`, {
+//         method: "PUT",
+//         body: JSON.stringify(formData),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
 
-      if (!response.ok) {
-        throw new Error("Failed to update user");
-      }
+//       if (!response.ok) {
+//         throw new Error("Failed to update user");
+//       }
 
-      navigate("/"); // Navigate back to the home page after updating
-    } catch (error) {
-      console.error("Error updating user:", error);
-    }
-  };
+//       navigate("/"); // Navigate back to the home page after updating
+//     } catch (error) {
+//       console.error("Error updating user:", error);
+//     }
+//   };
 
-  return (
-    <>
-      <div className="app-container">
-        <form onSubmit={sendReq} className="student-form">
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter the name"
-            value={formData.name}
-            onChange={handleChange}
-            className="form-input"
-          />
+//   return (
+//     <>
+//       <div className="app-container">
+//         <form onSubmit={sendReq} className="student-form">
+//           <input
+//             type="text"
+//             name="name"
+//             placeholder="Enter the name"
+//             value={formData.name}
+//             onChange={handleChange}
+//             className="form-input"
+//           />
 
-          <input
-            type="text"
-            name="age"
-            placeholder="Enter the age"
-            value={formData.age}
-            onChange={handleChange}
-            className="form-input"
-          />
-          <input
-            type="text"
-            name="image"
-            placeholder="Enter the img url"
-            value={formData.image}
-            onChange={handleChange}
-            className="form-input"
-          />
-          <button onClick={sendReq} className="form-button">
-            Update User
-          </button>
-        </form>
-      </div>
-    </>
-  );
-};
+//           <input
+//             type="text"
+//             name="age"
+//             placeholder="Enter the age"
+//             value={formData.age}
+//             onChange={handleChange}
+//             className="form-input"
+//           />
+//           <input
+//             type="text"
+//             name="image"
+//             placeholder="Enter the img url"
+//             value={formData.image}
+//             onChange={handleChange}
+//             className="form-input"
+//           />
+//           <button onClick={sendReq} className="form-button">
+//             Update User
+//           </button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// };
 
-export default Edit;
+// export default Edit;
